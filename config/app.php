063f1340ca71fd7c3cd468ba7ee886db68ddaba2
chenfeng.yao <282696845@qq.com>
2016-12-14 14:09:55 +0800

    l5

--- config/app.php
+++ config/app.php
@@ -123,7 +123,6 @@ return [
     'log' => env('APP_LOG', 'single'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
-    'repository.generator.stubsOverridePath'=> __DIR__,
 
     /*
     |--------------------------------------------------------------------------
